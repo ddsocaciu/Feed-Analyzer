dbee5cf618081263d740f6f3b6b1b6465e617b97
dsocaciu <dsocaciu@studen.tgm.ac.at>
2025-05-19 11:17:08 +0200

    Analyzed-Klasse fertiggestellt

--- TGMSEWFeedAnalyzer/AnalyzedText.java
+++ TGMSEWFeedAnalyzer/AnalyzedText.java
@@ -1,4 +1,83 @@
 package TGMSEWFeedAnalyzer;
 
-public class AnalyzedText {
+public class AnalyzedText implements Comparable<AnalyzedText>{
+
+    private String text;
+    private double sentiment;
+
+    public void AnalyzedText(String text) {
+        if(!text.equals("")){
+            this.text = text;
+        }else{
+            this.text = "mahrhofer";
+        }
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public double getSentiment(){
+        return sentiment;
+    }
+
+    @Override
+    public int compareTo(AnalyzedText o) {
+        if(o == null){
+            return -1;
+        }
+        if(this.equals(o)){
+            return 0;
+        }
+        int ret = this.text.compareTo(o.text);
+        if(ret != 0){
+            return ret;
+        }
+        ret = (int) (this.sentiment - o.sentiment);
+        if(ret != 0){
+            return ret;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        String txt = "";
+        txt+= "Text: " +getText()+", Sentiment:" +getSentiment();
+        return txt;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj){
+            return true;
+        }
+        if(obj == null){
+            return false;
+        }
+        if(this.getClass() != obj.getClass()){
+            return false;
+        }
+        AnalyzedText other = (AnalyzedText) obj;
+        if(this.text == null) {
+            if(other.text != null){
+                return false;
+            }
+        }else if(!this.text.equals(other.text)){
+            return false;
+        }
+        if(this.sentiment != other.sentiment){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + ((text == null) ? 0 : text.hashCode());
+        result = prime * result + (int) sentiment;
+        return result;
+    }
 }
