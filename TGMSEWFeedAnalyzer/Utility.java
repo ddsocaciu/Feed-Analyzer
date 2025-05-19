2e377b3ba99508395078e9d6ee0c2e2d48346a98
dsocaciu <dsocaciu@studen.tgm.ac.at>
2025-05-19 11:16:50 +0200

    Utility-Klasse fertiggestellt

--- TGMSEWFeedAnalyzer/Utility.java
+++ TGMSEWFeedAnalyzer/Utility.java
@@ -1,8 +1,41 @@
 package TGMSEWFeedAnalyzer;
+import java.io.*;
+import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Utility {
 
     public double analyzeText(String text) {
-        return 0;
+        double gesamtscore = 0;
+        Set<String> stopSet = new HashSet<>();
+        Map<String, Double> vaderMap = new HashMap<>();
+        try(BufferedReader br = new BufferedReader(new FileReader("./Resources/SmartStoplist.txt"))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                stopSet.add(line);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        try(BufferedReader br = new BufferedReader(new FileReader("./Resources/vader_lexicon.txt"))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("\t");
+                vaderMap.put(parts[0], Double.parseDouble(parts[1]));
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+        String[] textAbstand = text.split(" ");
+        for(String s : textAbstand){
+            if(vaderMap.containsKey(s) && !stopSet.contains(s)){
+                gesamtscore += vaderMap.get(s);
+            }
+        }
+        return gesamtscore;
     }
 }
